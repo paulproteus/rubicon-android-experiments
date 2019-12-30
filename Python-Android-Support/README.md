7cceb7e7ffd0b3ae6300d6ee1169087380bc1f1b
Asheesh Laroia <asheesh@asheesh.org>
2019-12-30 13:37:46 -0800

    Add Python 3.7 build script

---
+++ Python-Android-Support/README.md
@@ -0,0 +1,7 @@
+This directory is conceptually very loosely modeled off
+https://github.com/beeware/Python-Linux-support/ .
+
+Right now, it uses Python 3.7, but we can change that in the future.
+
+Right now, it hard-codes just the x86-64 ABI for Android, but we can
+change that in the future.
---
+++ Python-Android-Support/build.sh
@@ -0,0 +1,20 @@
+export HOST_TAG=linux-x86_64
+export TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/$HOST_TAG
+export TARGET=x86_64-linux-android
+export ANDROID_SDK_VERSION=29
+export AR=$TOOLCHAIN/bin/$TARGET-ar
+export AS=$TOOLCHAIN/bin/$TARGET-as
+export CC=$TOOLCHAIN/bin/${TARGET}${ANDROID_SDK_VERSION}-clang
+export CXX=$TOOLCHAIN/bin/${TARGET}${ANDROID_SDK_VERSION}-clang++
+export LD=$TOOLCHAIN/bin/$TARGET-ld
+export RANLIB=$TOOLCHAIN/bin/$TARGET-ranlib
+export STRIP=$TOOLCHAIN/bin/$TARGET-strip
+export READELF=$TOOLCHAIN/bin/$TARGET-readelf
+export CFLAGS="-fPIC -Wall -O0 -g"
+cd Python-3.7.6
+./configure --host "$TARGET" --build "$TARGET""$ANDROID_SDK_VERSION" --enable-shared \
+  --enable-ipv6 ac_cv_file__dev_ptmx=yes \
+  ac_cv_file__dev_ptc=no --without-ensurepip ac_cv_little_endian_double=yes \
+  --prefix=$PWD/Python-3.7.6-built
+make
+make install
