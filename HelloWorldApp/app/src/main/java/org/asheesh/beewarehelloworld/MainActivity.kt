6158f80dfd8128a2886a8acb4bd5be36de02f2dd
Asheesh Laroia <asheesh@asheesh.org>
2020-01-08 16:56:06 -0800

    Add random stabbing to find out why Python is not launching

--- HelloWorldApp/app/src/main/java/org/asheesh/beewarehelloworld/MainActivity.kt
+++ HelloWorldApp/app/src/main/java/org/asheesh/beewarehelloworld/MainActivity.kt
@@ -9,9 +9,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 import org.beeware.rubicon.Python
 import java.io.File
 import java.io.FileOutputStream
+import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
+import java.nio.file.attribute.FileAttribute
+import java.nio.file.attribute.PosixFilePermission
+import java.nio.file.attribute.PosixFilePermissions
 import java.util.stream.Stream
 import java.util.zip.ZipInputStream
 
@@ -31,10 +35,66 @@ class MainActivity : AppCompatActivity() {
         System.loadLibrary("rubicon")
         //System.loadLibrary("ffi")
 
+        if (true) {
+            // Experiment with launching Python manually
+            val python = File(applicationContext.dataDir!!.absolutePath + "/bin/python3.7")
+            if (python.exists()) {
+                python.setExecutable(true)
+                python.setReadable(true)
+            } else {
+                throw Error("yikes python is gone wtf")
+            }
+            val ownerWritable =
+                PosixFilePermissions.fromString("rwxr-xr-x")
+            val permissions: FileAttribute<*> =
+                PosixFilePermissions.asFileAttribute(ownerWritable)
+            /*Os.setenv(
+                "LD_LIBRARY_PATH",
+                applicationContext.dataDir!!.absolutePath + "/lib",
+                true
+            )*/
+            val pythonPath = applicationContext.dataDir!!.absolutePath + "/bin/python3.7"
+            Files.setPosixFilePermissions(
+                Paths.get(pythonPath),
+                PosixFilePermissions.fromString("rwxr-xr-x")
+            )
+            println("pythonpath = $pythonPath")
+            val shellCode =
+                "cd " + applicationContext.dataDir!!.absolutePath + "/bin ; " + "LD_LIBRARY_PATH=" + applicationContext.dataDir!!.absolutePath + "/lib" + " ./python3.7 -V"
+            println("shellCode = $shellCode")
+            val cmdArray =
+                arrayOf(
+                    "/system/bin/sh",
+                    "-c",
+                    shellCode
+                )
+            val p = Runtime.getRuntime().exec(cmdArray)
+            val pResult = p.waitFor()
+            println("result = $pResult")
+            var filePerm: Set<PosixFilePermission?>? = null
+            try {
+                filePerm =
+                    Files.getPosixFilePermissions(Paths.get(applicationContext.dataDir!!.absolutePath + "/bin/python3"))
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            val permission: String = PosixFilePermissions.toString(filePerm)
+            println("permission = $permission")
+            val iostat =
+                ProcessBuilder().inheritIO().command(
+                    "/system/bin/sh",
+                    "-c",
+                    applicationContext.dataDir!!.absolutePath + "/bin/python3.7 -V"
+                ).start()
+            val exitCode = iostat.waitFor()
+            println("exitCode = $exitCode")
+            return
+        }
+
         // Get asset
-        if (false) {
+        if (true) {
             val destDir = applicationContext.dataDir
-            val zis = ZipInputStream(assets.open("pythonhome.zip"))
+            val zis = ZipInputStream(assets.open("pythonhome-arch-indep.zip"))
             var zipEntry = zis.nextEntry
             val buf = ByteArray(1024)
             while (zipEntry != null) {
@@ -55,6 +115,13 @@ class MainActivity : AppCompatActivity() {
             }
             zis.closeEntry()
             zis.close()
+            val python = File(applicationContext.dataDir!!.absolutePath + "/bin/python3")
+            if (python.exists()) {
+                python.setExecutable(true)
+                python.setReadable(true)
+            } else {
+                throw Error("yikes python is gone wtf")
+            }
         }
         // Unpack Python into cache directory -- use applicationContext.externalCacheDir
         Os.setenv(
@@ -67,6 +134,7 @@ class MainActivity : AppCompatActivity() {
             "python home",
             applicationContext.dataDir!!.absolutePath
         )
+        Os.setenv("TMPDIR", applicationContext.cacheDir!!.absolutePath, true)
 
         /* if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(
@@ -91,7 +159,7 @@ class MainActivity : AppCompatActivity() {
             Log.w("hi", "hello, python is alive")
             // TODO: Call Python.eval() but no such function exists, rofl.
             Python.run(
-                applicationContext.dataDir!!.absolutePath + "/lib/helloworld.py",
+                applicationContext.dataDir!!.absolutePath + "/lib/testme.py",
                 arrayOf()
             )
             Log.w("hi", "hello, python is alive and ran hello world")
